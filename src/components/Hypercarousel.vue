f569e5cff1fa35622f49a6524d57b39926278d44
Steev Hise <steev@datamassage.com>
2025-02-28 21:17:51 -0700

    Now videos stop playing when they slide out of view.

--- src/components/Hypercarousel.vue
+++ src/components/Hypercarousel.vue
@@ -15,27 +15,28 @@
             reverse-transition="fade-transition"
             transition="fade-transition"
             interval=9000
-            cycle
-            height="1000"
+            xcycle
+            height=1000
           >
             <v-carousel-item v-for="(item,i) in filteredItems"
                             :key="i">
-              <v-card>
+              <v-card >
                 <v-card-item padding="0" margin="0">
                   <v-card-title>{{ item.caption }}</v-card-title>
                   <v-card-text>{{ item.text }}</v-card-text>
 
                 </v-card-item>
                 <v-img v-if="item.src.search(/.jpg|jpeg|png$/) > 0" :src=item.src :cover=false width="99%"></v-img>
-                <template v-if="item.src.endsWith('.mp4')">
+                <template v-if="item.src.endsWith('.mp4')" >
 <!--                  TODO: make this not cycle if the video is playing?-->
-                  <video controls  height="540">
+                  <video :id="`video${i}`" controls height="540"  preload="auto" >
                     <source :src="item.src" type="video/mp4" />
                   </video>
                 </template>
               </v-card>
             </v-carousel-item>
         </v-carousel>
+<!--        cycle? {{ interval }} {{ !!cycle }}-->
 
       </v-col>
     </v-row>
@@ -79,27 +80,42 @@ export default {
   computed: {
     filteredItems() {
       return this.items.filter((e,self) => {
-        console.log(self);
+        console.debug(self);
         return !this.tag || e.category == this.tag;
       });
+    },
+    vidVisible(id: string) {
+      const el = document.getElementById(id);
+      if(!el) {
+        return false;
+      }
+      console.log(el);
+      return true;
+
     }
   },
-  // methods: {
-  //   // returns an object in the shape that VuetifyPlayer wants.
-  //   videoSource(src: string) {
-  //     return {
-  //       sources: [
-  //         {
-  //           src,
-  //           type: 'video.mp4',
-  //         }
-  //       ],
-  //     }
-  //   }
-  // },
+  methods: {
+    videoOff(item: any) {   // just turn off any video that's playing.
+      const id = 'video' + item;
+      const videos: HTMLCollection = document.getElementsByTagName('video');
+      console.log(videos);
+      if(videos.length > 0) {
+        const vidArray = Array.from(videos);
+        vidArray.map( (video: any) => {
+          console.log(video);
+          console.log('playing! ', video.id, '  - pausing');
+          video.pause();    // will this really work?
+
+        })
+      }
+    }
+  },
   watch: {
-    tag(value) {
+    tag() {
       this.carousel = 0;
+    },
+    carousel() {
+      this.videoOff(this.carousel);
     }
   }
 };
@@ -107,8 +123,10 @@ export default {
 
 
 <style scoped>
-  body {
-    background-color: #999999;
-  }
+ video {
+   object-fit: fill;
+   padding: 5px;
+   margin: 5px;
+ }
 
 </style>
